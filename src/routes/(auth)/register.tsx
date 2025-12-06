d9f647ebf043a352a00185def251a872584f5afe
luthfiahmdf <luthfiahmdf@gmail.com>
2025-12-06 18:03:54 +0700

    delete:some-error

--- src/routes/(auth)/register.tsx
+++ src/routes/(auth)/register.tsx
@@ -1,95 +1,94 @@
-import { Link, createFileRoute, useRouter } from '@tanstack/react-router'
-import Cookies from 'js-cookie'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { useLogin, useRegister } from 'hooks/hooks'
-import type { TAuthSchema } from '@/lib/validations/auth.schema'
-import { authSchema } from '@/lib/validations/auth.schema'
-import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+// import { Link, createFileRoute, useRouter } from '@tanstack/react-router'
+// import Cookies from 'js-cookie'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useForm } from 'react-hook-form'
+// import type { TAuthSchema } from '@/lib/validations/auth.schema'
+// import { authSchema } from '@/lib/validations/auth.schema'
+// import { Button } from '@/components/ui/button'
+// import {
+//   Card,
+//   CardContent,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from '@/components/ui/card'
 
-import { Form } from '@/components/ui/form'
-import { AuthForm } from '@/components/ui/shared/auth-form'
-import { AuthHeader } from '@/components/ui/shared/auth-header'
+// import { Form } from '@/components/ui/form'
+// import { AuthForm } from '@/components/ui/shared/auth-form'
+// import { AuthHeader } from '@/components/ui/shared/auth-header'
 
-export const Route = createFileRoute('/(auth)/register')({
-  component: RegisterPage,
-})
+// export const Route = createFileRoute('/(auth)/register')({
+//   component: RegisterPage,
+// })
 
-function RegisterPage() {
-  const signUpUserForm = useForm<TAuthSchema>({
-    resolver: zodResolver(authSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  })
-  //   const router = useRouter()
-  const { mutate } = useRegister()
+// function RegisterPage() {
+//   const signUpUserForm = useForm<TAuthSchema>({
+//     resolver: zodResolver(authSchema),
+//     defaultValues: {
+//       email: '',
+//       password: '',
+//     },
+//   })
+//   //   const router = useRouter()
+//   const { mutate } = useRegister()
 
-  const handleSubmit = (values: TAuthSchema) => {
-    try {
-      mutate(values, {
-        onSuccess: () => {
-          alert('Registrasi berhasil! Silakan login dengan akun Anda.')
-          signUpUserForm.reset()
-        },
-      })
-    } catch (err) {
-      throw new Error('Terjadi kesalahan saat login. Silakan coba lagi.')
-    }
-  }
+//   const handleSubmit = (values: TAuthSchema) => {
+//     try {
+//       mutate(values, {
+//         onSuccess: () => {
+//           alert('Registrasi berhasil! Silakan login dengan akun Anda.')
+//           signUpUserForm.reset()
+//         },
+//       })
+//     } catch (err) {
+//       throw new Error('Terjadi kesalahan saat login. Silakan coba lagi.')
+//     }
+//   }
 
-  return (
-    <>
-      <AuthHeader />
-      <main className="flex min-h-screen gap-3 flex-col items-center justify-center bg-background">
-        <Card className="bg-[#FFFAF0] max-w-screen">
-          <CardHeader>
-            <CardTitle>
-              <h1 className="text-2xl font-normal ">Daftar</h1>{' '}
-              <p className="text-sm text-muted-foreground font-medium">
-                Masukkan kredensial Anda
-              </p>
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Form {...signUpUserForm}>
-              <AuthForm onSubmit={handleSubmit} />
-            </Form>
-          </CardContent>
-          <CardFooter className="flex flex-col gap-2">
-            <Button
-              type="submit"
-              form="authform"
-              className="w-full"
-              disabled={signUpUserForm.formState.isSubmitting}
-            >
-              {signUpUserForm.formState.isSubmitting
-                ? 'Logging in...'
-                : 'Daftar'}
-            </Button>
+//   return (
+//     <>
+//       <AuthHeader />
+//       <main className="flex min-h-screen gap-3 flex-col items-center justify-center bg-background">
+//         <Card className="bg-[#FFFAF0] max-w-screen">
+//           <CardHeader>
+//             <CardTitle>
+//               <h1 className="text-2xl font-normal ">Daftar</h1>{' '}
+//               <p className="text-sm text-muted-foreground font-medium">
+//                 Masukkan kredensial Anda
+//               </p>
+//             </CardTitle>
+//           </CardHeader>
+//           <CardContent>
+//             <Form {...signUpUserForm}>
+//               <AuthForm onSubmit={handleSubmit} />
+//             </Form>
+//           </CardContent>
+//           <CardFooter className="flex flex-col gap-2">
+//             <Button
+//               type="submit"
+//               form="authform"
+//               className="w-full"
+//               disabled={signUpUserForm.formState.isSubmitting}
+//             >
+//               {signUpUserForm.formState.isSubmitting
+//                 ? 'Logging in...'
+//                 : 'Daftar'}
+//             </Button>
 
-            <div className="text-center mt-6">
-              <p>
-                Sudah punya akun?{' '}
-                <Link
-                  to="/login"
-                  className="text-[#118AB2] font-bold hover:underline"
-                >
-                  Masuk sekarang
-                </Link>
-              </p>
-            </div>
-          </CardFooter>
-        </Card>
-      </main>
-    </>
-  )
-}
+//             <div className="text-center mt-6">
+//               <p>
+//                 Sudah punya akun?{' '}
+//                 <Link
+//                   to="/login"
+//                   className="text-[#118AB2] font-bold hover:underline"
+//                 >
+//                   Masuk sekarang
+//                 </Link>
+//               </p>
+//             </div>
+//           </CardFooter>
+//         </Card>
+//       </main>
+//     </>
+//   )
+// }
